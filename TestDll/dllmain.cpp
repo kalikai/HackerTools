48883d4a0b44b84e4d774ada225a33d0eb3489db
471934019@qq.com <471934019@qq.com>
2019-04-14 23:55:11 +0800

    updata

--- TestDll/dllmain.cpp
+++ TestDll/dllmain.cpp
@@ -10,19 +10,20 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+		system("start notepad");
 		MessageBoxA(0, "DLL_PROCESS_ATTACH", "ATTACH", 0);
 		break;
     case DLL_THREAD_ATTACH:
 		//此处注入增加打开应用程序
-		system("start notepad");
-		system("start calc");
 		system("start mspaint");
 		MessageBoxA(0, "DLL_THREAD_ATTACH", "ATTACH", 0);
 		break;
     case DLL_THREAD_DETACH:
+		system("start mspaint");
 		MessageBoxA(0, "DLL_THREAD_DETACH", "DETACH", 0);
 		break;
     case DLL_PROCESS_DETACH:
+		system("start cmd");
 		MessageBoxA(0, "DLL_PROCESS_DETACH", "DETACH", 0);
 		break;
 	default:
